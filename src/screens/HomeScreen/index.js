edcd323b851ded4548cab60120956ae7c92f555c
한광재 <hkj4900@pusan.ac.kr>
2020-07-17 23:58:31 +0900

    font 수정

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -7,16 +7,19 @@ import {
     Alert,
     StyleSheet
 } from 'react-native';
+import {Fonts} from '../../Fonts.js';
 
 
 export default class HomeScreen extends Component{
     render(){
         return (
             <View style={styles.container}>
-                <View style={styles.header}/>
-                <View style={styles.title} />
-                <View style={styles.content} />
-                <View style={styles.footer} />
+                <View style={styles.headerView}/>
+                <View style={styles.titleView}>
+                    <Text style={styles.titleStyle}>HWAPP</Text>
+                </View>
+                <View style={styles.contentView} />
+                <View style={styles.footerView} />
             </View>
             
         );
@@ -30,31 +33,35 @@ const styles = StyleSheet.create({
         //alignItems: 'center',
         backgroundColor: 'black',
       },
-      header: {
+      headerView: {
         width:'100%',
         height:'9%',
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#ff9a9a',
       },
-      title: {
+      titleView: {
         width:'100%',
-        height:'18%',
+        height:'15%',
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#9aa9ff',
       },
-      content: {
+      contentView: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#d6ca1a',
       },
-      footer: {
+      footerView: {
         width:'100%',
         height:'20%',
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#1ad657',
       },
+      titleStyle: {
+          fontSize: 70,
+          fontFamily: "BMDOHYEON"
+      }
 })
