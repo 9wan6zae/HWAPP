e43e5e40eb28b48c80a3ab3610ff91a591d6c959
한광재 <hkj4900@pusan.ac.kr>
2020-08-13 17:42:10 +0900

    axios 통신을 이용해 kitInfo 가져와서 테이블로 표시

--- App.js
+++ App.js
@@ -1,3 +1,157 @@
+// import React, {Component} from 'react';
+// import {
+//   View,
+//   Text,
+//   ScrollView,
+//   StyleSheet,
+//   StatusBar,
+//   Platform,
+// } from 'react-native';
+// import {Table} from 'react-native-table-component';
+// import {createAppContainer} from 'react-navigation';
+// import {createStackNavigator} from 'react-navigation-stack';
+
+// import CustomButton from './src/styles/CustomButton';
+// import KitInfoButton from './src/styles/KitItem';
+
+// import KitInfo from './src/screens/KitInfo';
+// import RegisterKitScreen from './src/screens/RegisterKitScreen';
+
+// const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
+
+// class HomeScreen extends Component {
+//   render() {
+//     var {navigate} = this.props.navigation;
+
+//     const tableData = [];
+//     for (let i = 0; i < 30; i += 1) {
+//       const rowData = [];
+//       for (let j = 0; j < 1; j += 1) {
+//         rowData.push(`${i}${j}`);
+//       }
+//       tableData.push(rowData);
+//     }
+
+//     return (
+//       <View style={styles.container}>
+//         <View style={styles.statusBar}>
+//           <StatusBar backgroundColor="#fff" barStyle="dark-content" ax />
+//         </View>
+//         <View style={styles.headerView} />
+//         <View style={styles.titleView}>
+//           <Text style={styles.titleStyle}>HWAPP</Text>
+//         </View>
+//         <View style={styles.contentView}>
+//           <View style={styles.registerView}>
+//             <CustomButton
+//               buttonColor={'#023e71'}
+//               title={'키트 등록하기'}
+//               onPress={() => navigate('RegisterKitScreen')}
+//             />
+//           </View>
+//           <View style={styles.tableContainer}>
+//             <View>
+//               <ScrollView style={styles.dataWrapper}>
+//                 <Table borderStyle={styles.borderStyle}>
+//                   {tableData.map((rowData, index) =>
+//                     rowData.map((cellData, cellIndex) => (
+//                       <KitInfoButton
+//                         buttonColor={'#3F3F3F'}
+//                         title={'키트 ' + (index + 1)}
+//                         onPress={() => navigate('KitInfo')}
+//                       />
+//                     )),
+//                   )}
+//                 </Table>
+//               </ScrollView>
+//             </View>
+//           </View>
+//         </View>
+//         <View style={styles.footerView} />
+//       </View>
+//     );
+//   }
+// }
+
+// const AppNavigator = createStackNavigator(
+//   {
+//     HomeScreen,
+//     RegisterKitScreen,
+//     KitInfo,
+//   },
+//   {
+//     defaultNavigationOptions: () => ({
+//       headerShown: false,
+//       headerTintColor: '#000',
+//       headerStyle: {
+//         backgroundColor: '#FFF',
+//       },
+//       headerBackTitle: ' ',
+//     }),
+//     initialRouteName: 'HomeScreen',
+//   },
+// );
+// export default createAppContainer(AppNavigator);
+
+// const styles = StyleSheet.create({
+//   statusBar: {
+//     height: STATUSBAR_HEIGHT,
+//   },
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     //alignItems: 'center',
+//     backgroundColor: '#FFF',
+//   },
+//   headerView: {
+//     width: '100%',
+//     height: '10%',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   titleView: {
+//     width: '100%',
+//     height: '10%',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     //backgroundColor: '#9aa9ff',
+//   },
+//   contentView: {
+//     flex: 1,
+//     alignItems: 'center',
+//     //backgroundColor: '#d6ca1a',
+//     flexDirection: 'column',
+//   },
+//   registerView: {
+//     marginTop: 20,
+//     height: '12%',
+//     width: '96%',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     //backgroundColor: '#FF0000',
+//   },
+//   footerView: {
+//     width: '100%',
+//     height: '2%',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     // backgroundColor: '#1ad657',
+//   },
+//   titleStyle: {
+//     fontSize: 70,
+//     fontFamily: 'NanumSquareRoundB',
+//   },
+//   tableContainer: {width: '100%', flex: 1, padding: 10, paddingTop: 30},
+//   header: {height: 50, backgroundColor: '#537791'},
+//   text: {textAlign: 'center', fontWeight: '100'},
+//   dataWrapper: {marginTop: -1},
+//   row: {height: 40, backgroundColor: '#E7E6E1'},
+//   borderStyle: {
+//     borderWidth: 1,
+//     borderColor: '#C1C0B9',
+//   },
+// });
+
 import React, {Component} from 'react';
 import {
   View,
@@ -7,6 +161,9 @@ import {
   StatusBar,
   Platform,
 } from 'react-native';
+
+import axios from 'axios';
+
 import {Table} from 'react-native-table-component';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
@@ -20,15 +177,31 @@ import RegisterKitScreen from './src/screens/RegisterKitScreen';
 const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
 
 class HomeScreen extends Component {
+  state = {
+    kitInfo: [],
+  };
+
+  loadKitInfo = async () => {
+    axios
+      .get('https://hwapp-2020.herokuapp.com/kit/getKitinfo?userId=faker')
+      .then((response) => this.setState({kitInfo: response.data}));
+  };
+
+  componentDidMount() {
+    this.loadKitInfo();
+  }
+
   render() {
+    const {kitInfo} = this.state;
+    console.log(kitInfo);
+
     var {navigate} = this.props.navigation;
 
     const tableData = [];
-    for (let i = 0; i < 30; i += 1) {
+    for (let i = 0; i < kitInfo.length; i += 1) {
       const rowData = [];
-      for (let j = 0; j < 1; j += 1) {
-        rowData.push(`${i}${j}`);
-      }
+      rowData.push(`${i}`);
+
       tableData.push(rowData);
     }
 
@@ -54,10 +227,10 @@ class HomeScreen extends Component {
               <ScrollView style={styles.dataWrapper}>
                 <Table borderStyle={styles.borderStyle}>
                   {tableData.map((rowData, index) =>
-                    rowData.map((cellData, cellIndex) => (
+                    rowData.map(() => (
                       <KitInfoButton
                         buttonColor={'#3F3F3F'}
-                        title={'키트 ' + (index + 1)}
+                        title={kitInfo[index].kitName}
                         onPress={() => navigate('KitInfo')}
                       />
                     )),
@@ -65,6 +238,9 @@ class HomeScreen extends Component {
                 </Table>
               </ScrollView>
             </View>
+          </View>
+          <View>
+            <Text>{this.state.children}</Text>
           </View>
         </View>
         <View style={styles.footerView} />
