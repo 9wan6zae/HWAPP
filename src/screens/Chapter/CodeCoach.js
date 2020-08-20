31550734f0e52e9f9a825cdbb3f8f4bb13266e51
한광재 <hkj4900@pusan.ac.kr>
2020-08-21 00:53:45 +0900

    API 연동 및 UI 수정(Page를 이용해서 설명할 코드를 보여줌)

--- src/screens/Chapter/CodeCoach.js
+++ src/screens/Chapter/CodeCoach.js
@@ -1,194 +1,92 @@
 import React, {Component} from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  ScrollView,
-} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import RBSheet from 'react-native-raw-bottom-sheet';
+import Axios from 'axios';
+import {Pages} from 'react-native-pages';
 
 export default class KitInfo extends Component {
   static navigationOptions = {
     title: '코드설명',
   };
+
+  state = {
+    pageIndex: '',
+  };
+
+  updateChapterStep = async () => {
+    const kitCode = this.props.navigation.state.params.kitCode;
+    Axios.patch('https://hwapp-2020.herokuapp.com/kit/updateChapterStep', {
+      userId: 'bang',
+      kitCode: kitCode,
+      step: 3,
+    });
+  };
+
+  componentDidMount() {
+    this.updateChapterStep();
+  }
+
+  RenderCodeIamge = (props) => {
+    if (props.index === 0) {
+      return <Image source={require('../../assets/Re.png')} />;
+    } else if (props.index === 1) {
+      return <Image source={require('../../assets/Fa.png')} />;
+    } else if (props.index === 2) {
+      return <Image source={require('../../assets/Su.png')} />;
+    }
+  };
+
+  RenderExplain = (props) => {
+    if (props.index === 0) {
+      return <Text>비밀번호 틀렸을 때 상태초기화</Text>;
+    } else if (props.index === 1) {
+      return <Text>도어락 잠김</Text>;
+    } else if (props.index === 2) {
+      return <Text>도어락 열림</Text>;
+    }
+  };
+
   render() {
+    var pages = [];
+    const maxPage = 3;
+    const {pageIndex} = this.state;
+    for (let i = 0; i < maxPage; i++) {
+      pages.push(
+        <View style={{flex: 1, backgroundColor: 'red'}}>
+          <View style={styles.contentView}>
+            <this.RenderCodeIamge index={i} />
+          </View>
+          <View style={styles.footerView}>
+            <TouchableOpacity
+              onPress={() => {
+                this.Explain.open();
+                this.setState({pageIndex: i});
+              }}
+              style={styles.explainButton}>
+              <Text style={styles.buttonTitle}>설명보기</Text>
+            </TouchableOpacity>
+          </View>
+        </View>,
+      );
+    }
+
     return (
       <View style={styles.container}>
-        <View style={styles.titleView} />
-        <View style={styles.contentView}>
-          <ScrollView style={styles.dataWrapper}>
-            <Text>
-              if (
-              <TouchableOpacity
-                onPress={() => this.Standard.open()}
-                style={styles.button}>
-                <Text style={styles.buttonTitle}>1</Text>
-              </TouchableOpacity>
-              ) {'{'} {'\n'}
-              {'\t'}test{'\n'}
-              {'}'}
-            </Text>
-            <Text>
-              if (
-              <TouchableOpacity
-                onPress={() => this.Standard.open()}
-                style={styles.button}>
-                <Text style={styles.buttonTitle}>1</Text>
-              </TouchableOpacity>
-              ) {'{'} {'\n'}
-              {'\t'}test{'\n'}
-              {'}'}
-            </Text>
-            <Text>
-              if (
-              <TouchableOpacity
-                onPress={() => this.Standard.open()}
-                style={styles.button}>
-                <Text style={styles.buttonTitle}>1</Text>
-              </TouchableOpacity>
-              ) {'{'} {'\n'}
-              {'\t'}test{'\n'}
-              {'}'}
-            </Text>
-            <Text>
-              if (
-              <TouchableOpacity
-                onPress={() => this.Standard.open()}
-                style={styles.button}>
-                <Text style={styles.buttonTitle}>1</Text>
-              </TouchableOpacity>
-              ) {'{'} {'\n'}
-              {'\t'}test{'\n'}
-              {'}'}
-            </Text>
-            <Text>
-              if (
-              <TouchableOpacity
-                onPress={() => this.Standard.open()}
-                style={styles.button}>
-                <Text style={styles.buttonTitle}>1</Text>
-              </TouchableOpacity>
-              ) {'{'} {'\n'}
-              {'\t'}test{'\n'}
-              {'}'}
-            </Text>
-            <Text>
-              if (
-              <TouchableOpacity
-                onPress={() => this.Standard.open()}
-                style={styles.button}>
-                <Text style={styles.buttonTitle}>1</Text>
-              </TouchableOpacity>
-              ) {'{'} {'\n'}
-              {'\t'}test{'\n'}
-              {'}'}
-            </Text>
-            <Text>
-              if (
-              <TouchableOpacity
-                onPress={() => this.Standard.open()}
-                style={styles.button}>
-                <Text style={styles.buttonTitle}>1</Text>
-              </TouchableOpacity>
-              ) {'{'} {'\n'}
-              {'\t'}test{'\n'}
-              {'}'}
-            </Text>
-            <Text>
-              if (
-              <TouchableOpacity
-                onPress={() => this.Standard.open()}
-                style={styles.button}>
-                <Text style={styles.buttonTitle}>1</Text>
-              </TouchableOpacity>
-              ) {'{'} {'\n'}
-              {'\t'}test{'\n'}
-              {'}'}
-            </Text>
-            <Text>
-              if (
-              <TouchableOpacity
-                onPress={() => this.Standard.open()}
-                style={styles.button}>
-                <Text style={styles.buttonTitle}>1</Text>
-              </TouchableOpacity>
-              ) {'{'} {'\n'}
-              {'\t'}test{'\n'}
-              {'}'}
-            </Text>
-            <Text>
-              if (
-              <TouchableOpacity
-                onPress={() => this.Standard.open()}
-                style={styles.button}>
-                <Text style={styles.buttonTitle}>1</Text>
-              </TouchableOpacity>
-              ) {'{'} {'\n'}
-              {'\t'}test{'\n'}
-              {'}'}
-            </Text>
-            <Text>
-              if (
-              <TouchableOpacity
-                onPress={() => this.Standard.open()}
-                style={styles.button}>
-                <Text style={styles.buttonTitle}>1</Text>
-              </TouchableOpacity>
-              ) {'{'} {'\n'}
-              {'\t'}test{'\n'}
-              {'}'}
-            </Text>
-            <Text>
-              if (
-              <TouchableOpacity
-                onPress={() => this.Standard.open()}
-                style={styles.button}>
-                <Text style={styles.buttonTitle}>1</Text>
-              </TouchableOpacity>
-              ) {'{'} {'\n'}
-              {'\t'}test{'\n'}
-              {'}'}
-            </Text>
-            <Text>
-              if (
-              <TouchableOpacity
-                onPress={() => this.Standard.open()}
-                style={styles.button}>
-                <Text style={styles.buttonTitle}>1</Text>
-              </TouchableOpacity>
-              ) {'{'} {'\n'}
-              {'\t'}test{'\n'}
-              {'}'}
-            </Text>
-            <Text>
-              if (
-              <TouchableOpacity
-                onPress={() => this.Standard.open()}
-                style={styles.button}>
-                <Text style={styles.buttonTitle}>1</Text>
-              </TouchableOpacity>
-              ) {'{'} {'\n'}
-              {'\t'}test{'\n'}
-              {'}'}
-            </Text>
-          </ScrollView>
-        </View>
-        <View style={styles.footerView} />
+        <Pages>{pages}</Pages>
         <RBSheet
           ref={(ref) => {
-            this.Standard = ref;
+            this.Explain = ref;
           }}
-          height={500}
+          height={700}
           openDuration={250}
           customStyles={{
             container: {
-              borderRadius: 30,
+              borderRadius: 20,
               justifyContent: 'center',
               alignItems: 'center',
             },
           }}>
-          <Text>처음 조건문</Text>
+          <this.RenderExplain index={pageIndex} />
         </RBSheet>
       </View>
     );
@@ -225,7 +123,7 @@ const styles = StyleSheet.create({
   },
   footerView: {
     width: '100%',
-    height: '10%',
+    height: '20%',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'white',
@@ -266,5 +164,13 @@ const styles = StyleSheet.create({
     marginTop: 0,
     marginLeft: 20,
     width: '95%',
+  },
+  explainButton: {
+    minWidth: 140,
+    height: 40,
+    backgroundColor: '#3AE5D1',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 3,
   },
 });
